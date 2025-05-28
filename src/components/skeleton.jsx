c85ccd68a0edc5386f23194039a2b3046c554356
Lakshay Setia <lakshaysetia123@gmail.com>
2025-05-28 08:15:51 +0530

    minor bug fix

--- src/components/skeleton.jsx
+++ src/components/skeleton.jsx
@@ -1,7 +1,7 @@
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
-const Skeleton = () => (
+const Loading = () => (
   <SkeletonTheme baseColor="#202020" highlightColor="#444">
     <div className="space-y-4 w-full max-w-md mx-auto">
       <Skeleton height={40} />
@@ -12,4 +12,4 @@ const Skeleton = () => (
   </SkeletonTheme>
 );
 
-export default Skeleton;
+export default Loading;
